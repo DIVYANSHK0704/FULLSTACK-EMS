afdbcb1da0b2808f01ccd09fb5f7aa98265a671f
DIVYANSHK0704 <divyanshk324@gmail.com>
2026-05-24 00:23:53 +0530

    login form ui completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginLanding from "./Pages/LoginLanding";
+import Layout from "./Pages/Layout";
+import Dashboard from "./Pages/Dashboard";
+import Employees from "./Pages/Employees";
+import Attendance from "./Pages/Attendance";
+import Leave from "./Pages/Leave";
+import PaySlips from "./Pages/PaySlips";
+import Settings from "./Pages/Settings";
+import PrintPaySlip from "./Pages/PrintPaySlip";
+import LoginForm from "./Components/LoginForm";
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
-  )
-}
+    <>
+      <Toaster />
+      <Routes>
 
-export default App
+        <Route path="/login" element={<LoginLanding />} />
+
+        <Route path="/login/admin" element={<LoginForm role="admin" title="Admin Portal" subtitle="Sign in to manage the organization"/>} />
+
+        <Route path="/login/employee" element={<LoginForm role="employee" title="Employee Portal" subtitle="Sign in to manage the organization"/>} />
+
+        <Route element={<Layout />}>
+
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/payslips" element={<PaySlips />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+
+        <Route path="/print/payslips/:id" element={<PrintPaySlip />} />
+
+        <Route path="*" element={<Navigate to="dashboard" replace />} />
+
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/Pages/PaySlips.jsx
@@ -0,0 +1,9 @@
+const PaySlips = () => {
+  return (
+    <div>
+      PaySlips
+    </div>
+  )
+}
+
+export default PaySlips
